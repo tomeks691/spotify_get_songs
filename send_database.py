923dfa26d250c24290e00b8e86cfddd25a45b71b
tomek <luci6969@protonmail.ch>
2022-01-15 13:37:21 +0100

    Add overwrite mode to dropbox files_upload

--- send_database.py
+++ send_database.py
@@ -1,5 +1,6 @@
 import dropbox
 import os
+from dropbox.files import WriteMode
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -8,4 +9,4 @@ token_access = os.environ.get("token_access")
 dbx = dropbox.Dropbox(token_access)
 
 with open(file_to_send, 'rb') as f:
-    dbx.files_upload(f=f.read(), path=f"/spotify/{file_to_send}")
+    dbx.files_upload(f=f.read(), path=f"/spotify/{file_to_send}", mode=WriteMode('overwrite'))
